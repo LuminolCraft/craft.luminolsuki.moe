53b1576cb0ff4d4fd7f3d8f27398c907096c6040
Narcssu <bling1112@outlook.com>
2026-05-26 03:54:10 +0800

    style: 调整导航栏样式与注释代码
    1. 移动端导航隐藏logo图片
    2. 调整桌面端导航链接左侧间距为5px
    3. 注释掉冗余的规则链接和离线pdf链接

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,14 +13,14 @@
           {{ t('common.home') }}
         </router-link>
         <router-link to="/SimpleRules" class="nav-link">
-          {{ t('common.rules') }} ({{ t('common.simpleRules') }})
+           {{ t('common.rules') }} <!--({{ t('common.rules') }}) -->
         </router-link>
-        <a class="dropdown-ico" href="https://docs.qq.com/pdf/DQUZYS0FKenFmYWZx" target="_blank" rel="noopener noreferrer">
+        <!-- <a class="dropdown-ico" href="https://docs.qq.com/pdf/DQUZYS0FKenFmYWZx" target="_blank" rel="noopener noreferrer">
           {{ t('common.rules') }} ({{ t('common.detailedRules') }})
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" class="external-link-icon" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
             </svg>
-        </a>
+        </a> -->
         <router-link to="/Support">
           {{ t('common.support') }}
         </router-link>
